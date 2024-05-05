8d547855c1aa7fea52866bf3e7f7c7b94e0183d7
GameHerobrine <waterfall009@gmail.com>
2024-05-05 07:12:52 +0300

    java random

---
+++ utils/javarandom.c
@@ -0,0 +1,36 @@
+#include "javarandom.h"
+static const long long int multiplier = 0x5DEECE66D;
+static const long long int mask = ((long long int)1 << 48) - 1;
+static const long long int addend = 0xB;
+
+void random_set_seed(Random* random, int seed){
+	random->seed = (seed ^ multiplier) & mask;
+}
+
+int random_next(Random* random, int bits){
+	long long int oldseed = 0, nextseed = 0;
+	long long int seed = random->seed;
+	
+	oldseed = seed;
+	nextseed = (oldseed * multiplier + addend) & mask;
+	
+	random->seed = nextseed;
+	
+	return (int)((unsigned long long int)nextseed >> (48 - bits));
+}
+
+int random_next_int(Random* random, int bound){
+	
+	int r = random_next(random, 31);
+	int m = bound - 1;
+	if(bound & m){
+		for(int u = r; u - (r = u % bound) + m < 0; u = random_next(random, 31));
+	}else{
+		r = ((bound * (long long int)r) >> 31);
+	}
+
+	return r;
+}
+float random_next_float(Random* random){
+	return random_next(random, 24) / (float)(1 << 24);
+}
--- utils/javarandom.h
+++ utils/javarandom.h
@@ -1,10 +1,14 @@
 #ifndef UTILS_JAVARANDOM_H_
 #define UTILS_JAVARANDOM_H_
 
-typedef struct Random{
-	int seed;
+typedef struct{
+	long long int seed;
 	float nextNextGaussian;
-};
+} Random;
 
+void random_set_seed(Random* random, int seed);
+int random_next(Random* random, int bits);
+int random_next_int(Random* random, int bound);
+float random_next_float(Random* random);
 
 #endif /* UTILS_JAVARANDOM_H_ */
